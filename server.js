18e481fec93022c3305fecdda13eb7d86fda1003
jashmehta3300 <jashmehta3300@gmail.com>
2020-02-24 20:09:06 +0530

    MongoDB connected

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-
-//Route files
-const hackathons = require('./routes/hackathons');
+const connectDB = require('./config/db');
 
 //load env vars
 dotenv.config({ path: './config/config.env' });
 
+//Connect to database
+connectDB();
+
 const app = express();
+
+//Route files
+const hackathons = require('./routes/hackathons');
 
 //Dev middleware Morgan
 if (process.env.NODE_ENV === 'development') {
@@ -23,9 +27,16 @@ app.use('/api/v1/hackathons', hackathons);
 //access env vars
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
     PORT,
     console.log(
         `The server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
     )
 );
+
+//Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    //close server and exit process
+    server.close(() => process.exit(1));
+});
