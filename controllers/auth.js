b4df1daf27349640fe88b999554257ff3f05848a
jashmehta3300 <jashmehta3300@gmail.com>
2020-02-27 18:07:53 +0530

    password encryption done

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,7 +4,18 @@ const User = require('../models/user');
 // @route     POST /api/v1/auth/register
 // @access    public
 exports.register = async(req, res, next) => {
+    const { name, email, password, role } = req.body;
+
+    //Create user
+    const user = await User.create({
+        name: name,
+        email: email,
+        password: password,
+        role: role
+    });
+
     res.status(200).json({
-        success: true
+        success: true,
+        data: user
     });
 };
