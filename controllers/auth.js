0c203af5d9f0b2de4656e11ec4b16e82c8c10992
jashmehta3300 <jashmehta3300@gmail.com>
2020-02-28 13:29:09 +0530

    generate JWT tokens

--- controllers/auth.js
+++ controllers/auth.js
@@ -14,8 +14,61 @@ exports.register = async(req, res, next) => {
         role: role
     });
 
+    //Create token
+    const token = user.getSignedJwtToken();
+
+    //So now we are encrypting the password and instead of returning the data, we are sending a JWT token back
+
     res.status(200).json({
         success: true,
-        data: user
+        token: token
+    });
+};
+
+// @desc      Login user
+// @route     POST /api/v1/auth/login
+// @access    Public
+exports.login = async(req, res, next) => {
+    const { email, password } = req.body;
+
+    // Validate emil & password
+    if (!email || !password) {
+        return next(
+            res.status(400).json({
+                success: false,
+                error: 'Please provide email and password'
+            })
+        );
+    }
+
+    // Check for user
+    const user = await User.findOne({ email }).select('+password');
+
+    if (!user) {
+        return next(
+            res.status(401).json({
+                success: false,
+                error: 'Invalid Credentials'
+            })
+        );
+    }
+
+    // Check if password matches
+    const isMatch = await user.matchPassword(password);
+
+    if (!isMatch) {
+        return next(
+            res.status(401).json({
+                success: false,
+                error: 'Invalid Credentials'
+            })
+        );
+    }
+
+    const token = user.getSignedJwtToken();
+
+    res.send(200).json({
+        success: true,
+        token
     });
 };
